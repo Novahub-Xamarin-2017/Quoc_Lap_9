189f905bba1c2aef31226899708cd6fb6a517f12
Steven <stevelee2497@gmail.com>
2017-12-12 10:59:32 +0700

    extract values

--- Exercise2Solution/Resources/Resource.Designer.cs
+++ Exercise2Solution/Resources/Resource.Designer.cs
@@ -41,23 +41,97 @@ namespace Exercise2Solution
 			}
 		}
 		
-		public partial class Id
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f030004
+			public const int black = 2130903044;
+			
+			// aapt resource value: 0x7f030002
+			public const int colorAccent = 2130903042;
+			
+			// aapt resource value: 0x7f030000
+			public const int colorPrimary = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int colorPrimaryDark = 2130903041;
+			
+			// aapt resource value: 0x7f030003
+			public const int pink = 2130903043;
+			
+			// aapt resource value: 0x7f030006
+			public const int result_backgound = 2130903046;
+			
+			// aapt resource value: 0x7f030005
+			public const int white = 2130903045;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
 		{
 			
 			// aapt resource value: 0x7f040003
-			public const int btSolve = 2130968579;
+			public const int edt_height = 2130968579;
 			
-			// aapt resource value: 0x7f040000
-			public const int etA = 2130968576;
+			// aapt resource value: 0x7f040008
+			public const int input_height = 2130968584;
 			
-			// aapt resource value: 0x7f040001
-			public const int etB = 2130968577;
+			// aapt resource value: 0x7f040009
+			public const int input_text_size = 2130968585;
 			
 			// aapt resource value: 0x7f040002
-			public const int etC = 2130968578;
+			public const int margin_left = 2130968578;
 			
 			// aapt resource value: 0x7f040004
-			public const int tvResult = 2130968580;
+			public const int normal_size = 2130968580;
+			
+			// aapt resource value: 0x7f040006
+			public const int result_margin_right = 2130968582;
+			
+			// aapt resource value: 0x7f040005
+			public const int result_text = 2130968581;
+			
+			// aapt resource value: 0x7f040001
+			public const int title_size = 2130968577;
+			
+			// aapt resource value: 0x7f040007
+			public const int title_text_size = 2130968583;
+			
+			// aapt resource value: 0x7f040000
+			public const int toolbar_navigation_text_size = 2130968576;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int etA = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int etB = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int etC = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int tvResult = 2131099651;
 			
 			static Id()
 			{
@@ -88,8 +162,20 @@ namespace Exercise2Solution
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f050002
+			public const int a = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int app_name = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int b = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int c = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int quadratic_equation_solver = 2131034113;
 			
 			static String()
 			{
