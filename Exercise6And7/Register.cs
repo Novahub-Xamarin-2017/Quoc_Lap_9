c3e0aff32418bc739a68fff0cb524b972c5850c5
Steven <stevelee2497@gmail.com>
2017-12-13 17:02:09 +0700

    Add solutions of exercise 6 and 7

---
+++ Exercise6And7/Register.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Lilarcor.Cheeseknife;
+
+namespace Exercise6And7
+{
+    [Activity(Label = "Register", Theme = "@style/MyTheme")]
+    public class Register : Activity
+    {
+        [InjectOnClick(Resource.Id.btnLogin)]
+        private void ReturnToMain(object sender, EventArgs e)
+        {
+            Finish();
+        }
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Register);
+            ActionBar.SetHomeButtonEnabled(true);
+            ActionBar.SetDisplayHomeAsUpEnabled(true);
+            Cheeseknife.Inject(this);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    Finish();
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+    }
+}
---
+++ Exercise6And7/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Exercise6And7
+{
+    [Activity(Label = "SplashActivity", MainLauncher = true, Theme = "@style/SplashTheme")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
